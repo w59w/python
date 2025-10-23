cee9ed680c63001d115d8bd16fffddb3c159561e
w59w <kumiwa5@icloud.com>
2025-10-23 20:27:24 +0600

    прошел 4 урок прошли переменные, типы данных и сейчас коммит на новый файл

---
+++ input.py
@@ -0,0 +1,18 @@
+#question1 = input("What is your name?")
+#print("Her name is", question1)
+#question2 = ("how old are you?")
+#print("she is", question2, "years old")
+
+#dialog
+hello = "hello, please tell me about yourself. firstly, what is your name?"
+print(hello)
+question2 = "how old are you?"
+print(question2)
+question3 = "what is your gender?"
+print(question3)
+
+#переменные
+num5 = 33.3
+type_of_num5 = type(num5)
+print(type_of_num5)
+print(type(num5))
