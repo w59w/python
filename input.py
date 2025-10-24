11c335dffb8c54cc9134895a7e1cb3d02c12dd68
w59w <kumiwa5@icloud.com>
2025-10-24 21:05:51 +0600

    24/10

--- input.py
+++ input.py
@@ -16,3 +16,13 @@ num5 = 33.3
 type_of_num5 = type(num5)
 print(type_of_num5)
 print(type(num5))
+
+name = "alice"
+age = 10
+print(f"my name is {name} and i am {age} years old.")
+
+hair_color = "blond"
+isA = "musician"
+print("my hair is", hair_color, "and i am a", isA)
+print(f"my hair color is {hair_color} and i am a {isA}.")
+
