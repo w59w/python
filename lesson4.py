7f770121e3e379bb5069853d0d749f7e5c4cde7d
w59w <kumiwa5@icloud.com>
2025-10-27 20:57:37 +0600

    lesson4

---
+++ lesson4.py
@@ -0,0 +1,27 @@
+student1 = ["tom", "sam", "bob", "tim", "sara"]
+student2 = ["alice", "john", "eva"]
+tim_index = student1.index("tim")
+print(student1[tim_index])
+student1[tim_index] = "timothy"
+print(student1)
+# student1.extend(student2)
+# student2.clear()
+# student1.clear()
+
+
+# print(len(student1))
+# student1.append("bob")
+# print(len(student1))
+# print(student1)
+#
+# names2 = student1[1:-1:2] #start stop step
+# print(names2)
+#
+# student1[4:] = student1[0:2]
+# # a, b, c = 2, 4, 6
+# #student1[-1] = student1[0]
+# #student1[-2] = student1[1]
+# print(student1)
+
+# student1.insert(0, 'Alex')
+# print(student1)
