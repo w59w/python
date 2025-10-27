70b8c85cee2101b95c4e7e397067858af6e36301
w59w <kumiwa5@icloud.com>
2025-10-27 19:15:19 +0600

    lesson 4

---
+++ lesson2.py
@@ -0,0 +1,28 @@
+#question1 = input("What is your name?")
+#print("Her name is", question1)
+#question2 = ("how old are you?")
+#print("she is", question2, "years old")
+
+#dialog
+hello = "hello, please tell me about yourself. firstly, what is your name?"
+print(hello)
+question2 = "how old are you?"
+print(question2)
+question3 = "what is your gender?"
+print(question3)
+
+#переменные
+num5 = 33.3
+type_of_num5 = type(num5)
+print(type_of_num5)
+print(type(num5))
+
+name = "alice"
+age = 10
+print(f"my name is {name} and i am {age} years old.")
+
+hair_color = "blond"
+isA = "musician"
+print("my hair is", hair_color, "and i am a", isA)
+print(f"my hair color is {hair_color} and i am a {isA}.")
+
---
+++ lesson3.py
@@ -0,0 +1,81 @@
+# # products = ['apple', 'bananas', 'eggs', 'milk']
+# # product2 = list()
+
+# # num1 = int(5.5)
+# # num1 = 1
+
+# # print(f"i want to buy {products}")
+# # print(products[1])
+# # print(products[-1])
+
+# # products2 = ['bread', 'chips', 'nuts', 'juice']
+# # print(f"fristly i bought {products2[2]}, then i bought {products[0]} and finally i bought {products2[0]}.")
+
+# price = input("enter the price: ")
+# # price = int(price)
+# print("change is:", 500 - int(price))
+# print("hello", 334, 888.555, sep="***", end="!!!\n")
+
+# # lesson3.py
+# # This is a simple dialog program that asks for user information and prints it out.
+
+# #вычислительные операции
+# sum_result = 5 + 3
+# print("5 + 3 =", sum_result)
+
+# diff_result = 10 - 4
+# print("10 - 4 =", diff_result)
+
+# prod_result = 7 * 6
+# print("7 * 6 =", prod_result)
+
+# div_result = 20 / 5
+# print("20 / 5 =", div_result)
+
+# mod_result = 22 % 7
+# print("22 % 7 =", mod_result)
+
+# exp_result = 2 ** 3
+# print("2 ** 3 =", exp_result)
+
+# floor_div_result = 15 // 4
+# print("15 // 4 =", floor_div_result)
+
+# #dialog and variables
+# greeting = "Hello! Let's get to know each other."
+# print(greeting)
+
+# name = "Alice"
+# age = 10
+# print(f"My name is {name} and I am {age} years old.")
+
+# hair_color = "blond"
+# profession = "musician"
+# print(f"My hair color is {hair_color} and I am a {profession}.")
+# print("My hair is", hair_color, "and I am a", profession)
+
+# # print(type_of_num5)
+# num5 = 33.3
+# type_of_num5 = type(num5)
+# print(type_of_num5)
+# print(type(num5))
+# # print(type(num5))
+
+# a = 5
+# b = 2
+# c = 10
+# result = a * b / c
+# print("The result of a * b / c is:", result)
+
+#task 1
+print(2*(3-1))
+print((5-1)*(5+1))
+print(0.3*(4-1))
+print((91 - 1)/(2+1))
+print(((5*4)/3)**1/2)
+print(1/3 + 5 * (0.2 - 0.001)/91)
+print((40-(4.25 * 7.08 + 6.768 / 0.75))*2050)
+print((0.16*(3.2 - 3/40) + 2 * 3/11 * 4.125 / 3 * 3/4)/(5 * 1/6 * 0.3 - 0.3 * 4.5 + 1/3 * 0.3))
+
+
+
