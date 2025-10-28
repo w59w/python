d183329b776bbc39b69ed1596902e4d20eb77683
w59w <kumiwa5@icloud.com>
2025-10-28 18:45:46 +0600

    hw1

---
+++ python for DS/homework1.py
@@ -0,0 +1,85 @@
+import math
+
+# task 1
+number1 = 40
+number2 = 20
+num1 = number1 + number2
+print(num1)
+
+#task 2
+num2 = int(number1 / number2)
+print(num2)
+
+#task 3
+num3 = number1 % number2
+print(num3)
+
+#task 4
+float_num1 = 6.33
+float_num2 = 2.54
+float_num3 = 3.45
+num4 = float_num1 * float_num2 / float_num3
+print(num4)
+
+# task 5
+n = 5.63
+num5 = round(n**2/2, 1)
+print(num5)
+
+#task 6
+number3 = 4.33
+number4 = 5.66
+num6 = math.floor(number3 - number4)
+print(num6)
+
+#task 7
+num7 = math.ceil(number3 - number4)
+
+#task 8
+a = 20
+b = 30
+c = (a**2 + b**2)**1/2
+num8 = c
+print(num8)
+
+# task 9
+pos_num = 5
+neg_num = -20
+num9 = abs(pos_num) + abs(neg_num)
+print(num9)
+
+#task 10
+temp = 90 #farenheit
+celcius = 5/9 * (temp - 32)
+num10 = round(celcius,2)
+print(num10)
+
+#task 11
+num11 = ((((6+7/12) - (3+17/36)) * 25 - (4+1/3) / 0.65)/(4/(1/4)-0.5))
+print(num11)
+
+#task12
+
+n1 = (2+3/4)/11+(3+1/3)
+n2 = 2.5-0.4 * (3+1/3)
+n3 = ((2+1/6)+4.5) * 0.375
+n4 = 2.75 - (1+1/2)
+num12 = ((n1/n2)/(5/7)) - (n3/n4)
+print(num12)
+
+#task 13
+num13 = (11+2/5)+(7+1/2)*(285.6 / 14 - (1+23/30)+(13/50))/(24.4-10.23)
+print(num13)
+
+#task14
+num14 = ((9-(5+3/8))*((4+5/12)-4/(2+2/3))+(3/10-0.5/4)*4/7)/(1/24 + 0.25 /(13+1/3))
+print(num14)
+
+# task 15
+num15 = 575 / 0.025
+print(num15)
+
+# task 16
+x = (0.16 * (3.2 - 3/40)+(2+3/11)*4.125/(3+3/4))/((5+1/6)*0.3-0.3*4.5+(1/3)*0.3)
+num16 = 0.4 * x
+print(num16)
