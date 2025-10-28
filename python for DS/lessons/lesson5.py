0702b45bfc8c8f51275b7659f8a85ec5efd66edc
w59w <kumiwa5@icloud.com>
2025-10-28 20:59:56 +0600

    lesson5

---
+++ python for DS/lessons/lesson5.py
@@ -0,0 +1,60 @@
+# x = 0.5
+# x = str(x)
+# print(type(x))
+
+# price = int(input("Enter price: "))
+# print(f"The price is {price}.")
+
+# a = True + True + False
+# print(a)
+# num1 = 2>1
+# num2 = 1==3
+# num3 = num1 != num2
+# print(num3)
+# b = num1 + num2
+# print(num1)
+# print(num2)
+
+# num1 = 4
+# num2 = 5
+# print(num1 <= num2)
+# print(num1 >= num2)
+
+# age = int(input("Enter your age: "))
+# a = age % 10 == 0 and age >= 50
+# print(a)
+
+# age = int(input("Enter your age: "))
+# a = age % 10 == 0 or age >= 50
+# print(a)
+
+# name = "Sara"
+# if name == "Bob":
+#     print("It is a male")
+# else:
+#     print("It is a female")
+
+# if 1 + 1 >= 2:
+#     print("Good JOB!")
+# else:
+#     print("Bad JOB!")
+
+# a = int(input("Enter a number: "))
+# if a > 0:
+#     print("The number is positive.")
+# elif a == 0:
+#     print("The number is a zero.")
+# else:
+#     print("The number is negative.")
+
+# a = int(input('Enter: '))
+# if (a > 0) or (a == 5):
+#     print("positive 5")
+# else:
+#     print("negative")
+
+# person = input("Enter status: ")
+# if person == "student" or person == "school 10th" or person == "school 11th":
+#     print("the persons age is above 15")
+# else:
+#     print("the persons age is below 15")
